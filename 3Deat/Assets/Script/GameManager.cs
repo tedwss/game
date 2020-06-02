824fb75efc720bddc9e4f38f5d1fa4b72e1b706d
tedwss <63327530+tedwss@users.noreply.github.com>
2020-06-02 11:08:51 +0800

    完成並發布網頁版

---
+++ 3Deat/Assets/Script/GameManager.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using UnityEngine.UI;               // UI API
+using UnityEngine.SceneManagement;  // 場景管理 API
+
+public class GameManager : MonoBehaviour
+{
+    #region 欄位與屬性
+    [Header("道具")]
+    public GameObject[] props;
+    [Header("文字介面 : 道具總數")]
+    public Text textCount;
+    [Header("文字介面 : 倒數時間")]
+    public Text textTime;
+    [Header("結束畫面標題")]
+    public Text textTitle;
+    [Header("結束畫面")]
+    public CanvasGroup final;
+
+    /// <summary>
+    /// 道具總數
+    /// </summary>
+    private int countTotal;
+
+    /// <summary>
+    /// 取得道具數量
+    /// </summary>
+    private int countProp;
+
+    /// <summary>
+    /// 遊戲時間
+    /// </summary>
+    private float gameTime = 30;
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 生成道具
+    /// </summary>
+    /// <param name="prop"></param> 想要生成的道具</param>
+    /// <param name="count"></param> 想要生成的數量 + 隨機值 + - 5</param>
+    /// <returns></returns>
+    private int CreateProp(GameObject prop, int count)
+    {
+        int total = count + Random.Range(-5, 5);
+        for(int i = 0; i < total; i++)
+        {
+            // 座標 = (隨機, 1.5, 隨機);
+            Vector3 pos = new Vector3(Random.Range(-8, 8), 1.5f, Random.Range(-8, 8));
+            // 生成(物件, 座標, 角度);
+            Instantiate(prop, pos, Quaternion.identity);
+        }
+        // 傳回 道具數量
+        return total;
+    }
+
+    /// <summary>
+    /// 時間倒數
+    /// </summary>
+    private void CountTime()
+    {
+        // 遊戲時間 遞減 一禎的時間
+        gameTime -= Time.deltaTime;
+
+        // 遊戲時間 = 數學.夾住(遊戲時間，最小值，最大值)
+        gameTime = Mathf.Clamp(gameTime, 0, 100);
+
+        // 更新倒數時間介面 ToString(f小數點位置);
+        textTime.text = "倒數時間 : " + gameTime.ToString("f2");
+
+        Lose();
+    }
+
+    /// <summary>
+    /// 取得道具 : 藍色汽油桶 - 更新數量與介面，黃色汽油桶 - 扣
+    /// </summary>
+    /// <param name="prop"></param>
+    public void GetProp(string prop)
+    {
+        if(prop == "藍色汽油桶")
+        {
+            countProp++;
+            textCount.text = "道具數量 : " + countProp + " / " + countTotal;
+
+            Win();          // 呼叫勝利方法
+        }
+        else if(prop == "黃色汽油桶")
+        {
+            gameTime -= 2;
+            textTime.text = "倒數時間 : " + gameTime.ToString("f2");
+
+        }
+    }
+
+    /// <summary>
+    /// 勝利 : 獲得所有藍色汽油桶
+    /// </summary>
+    private void Win()
+    {
+        if(countProp == countTotal)                             // 如果藍汽油桶數 = 藍汽油桶總數
+        {
+            final.alpha = 1;                                    // 顯示結束畫面、啟動互動、啟動遮擋
+            final.interactable = true;
+            final.blocksRaycasts = true;
+            textTitle.text = "恭喜你獲得所有藍色汽油桶";        // 更新結束畫面標題
+        }
+    }
+
+    /// <summary>
+    /// 失敗 : 時間歸零
+    /// </summary>
+    private void Lose()
+    {
+        if(gameTime == 0)
+        {
+            final.alpha = 1;                                    
+            final.interactable = true;
+            final.blocksRaycasts = true;
+            textTitle.text = "挑戰失敗!!!";
+            FindObjectOfType<player>().enabled = false;         // 取得玩家.啟動 = false
+        }
+    }
+
+    /// <summary>
+    /// 重新遊戲
+    /// </summary>
+    public void Replay()
+    {
+        SceneManager.LoadScene("3Deat");
+    }
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();     // 應用程式.離開()
+    }
+
+
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+        // 道具總數 = 生成道具(道具1號, 指定數量) // 道具1號都是從0開始 道具1號 = [0]
+        countTotal = CreateProp(props[0], 20);
+        CreateProp(props[1], 10);
+
+        textCount.text = "道具數量 : 0 / " + countTotal;
+    }
+
+    private void Update()
+    {
+        CountTime();
+    }
+    #endregion
+}
---
+++ 3Deat/Assets/Script/player.cs
@@ -0,0 +1,191 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    #region 欄位與屬性
+    [Header("移動速度"), Range(1, 1000)]
+    public float speed = 10;
+    [Header("跳躍高度"), Range(1, 5000)]
+    public float height;
+
+    /// <summary>
+    /// 是否在地板上
+    /// </summary>
+    public bool isGround
+    {
+        get
+        {
+            if (transform.position.y < 0.051f) return true; // 如果 Y軸 小於 0.051 傳回 true
+            else return false;                              // 否則 傳回 false
+        }
+    }
+
+    /// <summary>
+    /// 旋轉角度
+    /// </summary>
+    private Vector3 angle;
+
+    private Animator ani;       // 動畫
+    private Rigidbody rig;      // 剛體
+    private AudioSource aud;    // 喇叭
+    private GameManager gm;     // 遊戲管理器
+
+    /// <summary>
+    /// 跳躍力道 : 從 0 慢慢增加
+    /// </summary>
+    private float jump;
+
+    [Header("藍色汽油桶音效")]
+    public AudioClip soundBluebarrel;
+    [Header("黃色汽油桶音效")]
+    public AudioClip soundYellowbarrel;
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 移動：透過鍵盤
+    /// </summary>
+    private void Move() 
+    {
+        #region 移動
+        // 浮點數 前後值 = 輸入類別.取得軸向值("垂直") - 垂直 WS 上下
+        float v = Input.GetAxisRaw("Vertical");
+        // 水平 AD 左右
+        float h = Input.GetAxisRaw("Horizontal");
+        // 剛體.添加推力(x, y, z); - 世界座標
+        // rig.AddForce(0, 0, speed * v);
+        // 剛體.添加推力(三圍向量)
+        // 前方transform.forward - z
+        // 右方transform.right - x
+        // 上方transform.up - y
+        rig.AddForce(transform.forward * speed * Mathf.Abs(v));
+        rig.AddForce(transform.forward * speed * Mathf.Abs(h));
+        // 動畫.設定布林值("跑步參數", 布林值) - 當 前後取得絕對值 大於 0 時勾選
+        ani.SetBool("跑步開關", Mathf.Abs(v) > 0 || Mathf.Abs(h) > 0);
+        // ani.SetBool("跑步開關", v == 1 || v == -1); // 使用邏輯運算子
+        #endregion
+
+        #region 轉向
+        Vector3 angle = Vector3.zero; // 區域變數 - 只能在此結構{}內存取
+
+        if (v == 1) angle = new Vector3(0, 0, 0);               // 前 Y 0
+        else if (v == -1) angle = new Vector3(0, 180, 0);       // 後 Y 180
+        else if (h == 1) angle = new Vector3(0, 90, 0);         // 右 Y 90
+        else if (h == -1) angle = new Vector3(0, 270, 0);       // 左 Y 270
+        // 只要類別後面有 : Monobehaviour
+        // 就可以直接使用 transform 取得物件的 Transform 元件
+        // eulerAngle 歐拉角度 0 - 360
+        transform.eulerAngles = angle;
+        #endregion
+    }
+    /// <summary>
+    /// 跳躍：判斷在地板上並按下空白件時跳躍
+    /// </summary>
+    private void Jump() 
+    {
+        // 如果 在地板上 為 勾選 並且 按下空白鍵
+        if(isGround && Input.GetButtonDown("Jump"))
+        // 自訂跳躍按鍵
+        // if(isGround && Input.GetKeyDown(KeyCode.LeftAlt)) 
+        {
+            // 每次跳躍 值 都從 0 開始
+            jump = 0;
+            // 剛體.推力(0, 跳躍高度, 0);
+            rig.AddForce(0, height, 0);
+        }
+        // 如果 不在地板上(在空中)
+        if (!isGround)
+        {
+            jump += Time.deltaTime;
+        }
+        // 動畫.設定浮點數("跳躍參數"，跳躍時間)
+        ani.SetFloat("跳躍力道", jump);
+    }
+    /// <summary>
+    /// 碰到道具：碰到帶有標籤[汽油桶]的物件
+    /// </summary>
+    private void Hitprop(GameObject prop)
+    {
+        print("碰到物件的標籤為 : " + prop.name);
+
+        if(prop.tag == "藍色汽油桶")
+        {
+            aud.PlayOneShot(soundBluebarrel, 2);        // 喇叭.播放一次音效(音效片段，音量)
+            Destroy(prop);                              // 刪除(物件)
+        }
+        else if(prop.tag == "黃色汽油桶")
+        {
+            aud.PlayOneShot(soundYellowbarrel, 2);
+            Destroy(prop);
+        }
+
+        gm.GetProp(prop.tag);
+    }
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+        // GetComponent<泛型>　泛型方法 - 泛型 所有類型　Rigibody, Transform, Collider..
+        // 剛體(Rigidbody) = 取得元件<剛體>
+        rig = GetComponent<Rigidbody>();
+        ani = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
+        // FOOT 僅限於場景上只有一個類別存在使用
+        // 例如 : 場景上只有一個 GameManager 類別時可以使用他來取得
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    // 固定更新頻率事件：1秒50禎，使用物理必須在此事件內
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    // 更新事件 : 1秒約60禎
+    private void Update()
+    {
+        
+    }
+
+    // 碰撞事件 : 當物件碰撞開始時執行一次(沒有勾選 Is Trigger)
+    // collision 碰到物件的碰撞資訊
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    // 碰撞事件 : 當物件碰撞離開時執行一次(沒有勾選 Is Trigger)
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+
+    // 碰撞事件 : 當物件碰撞開始時持續執行(沒有勾選 Is Trigger) 60FPS
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    /* - - - - -*/
+    //觸發事件 : 當物件碰撞開始時執行一次(有勾選 Is Trigger)
+    private void OnTriggerEnter(Collider other)
+    {
+        // 碰到道具(碰撞資訊.遊戲物件)
+        Hitprop(other.gameObject);
+    }
+
+    //觸發事件 : 當物件碰撞離開時執行一次(有勾選 Is Trigger)
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+    //觸發事件 : 當物件碰撞開始時持續執行(有勾選 Is Trigger)
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+    #endregion
+}
